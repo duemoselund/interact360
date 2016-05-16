331e205e17dbb57388b1985f9178d0bc57a41a75
Anders Lund <Anders Lund>
2016-05-17 00:41:32 +0200

    Added horizontally flipped videos to compensate for the reversed sphereTexture

--- Interact360/Assets/Scripts/Navigation.cs
+++ Interact360/Assets/Scripts/Navigation.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class Navigation : MonoBehaviour {
 
     public Texture[] roomTextures;
-    public GameObject sphere;
+    public GameObject[] cameraPositions;
+    public GameObject sphere360;
+    public GameObject parentCamera;
     int arryPos = 1;
 
 	// Use this for initialization
@@ -16,10 +18,13 @@ public class Navigation : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
             if (arryPos == roomTextures.Length) arryPos = 0;
-            Renderer sphereRender = sphere.GetComponent<Renderer>();
+            Renderer sphereRender = sphere360.GetComponent<Renderer>();
             sphereRender.material.mainTexture = roomTextures[arryPos];
+            sphere360.transform.parent.transform.position = cameraPositions[arryPos].transform.position;
+            parentCamera.transform.position = cameraPositions[arryPos].transform.position;
 
             MovieTexture movie = (MovieTexture)sphereRender.material.mainTexture;
+
             movie.Play();
             movie.loop = true;
             arryPos++;
